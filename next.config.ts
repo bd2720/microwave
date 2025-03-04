079cd996c340e386e599e190c854cfa60a7622be
bd2720 <brendandeneen9@gmail.com>
2025-03-03 22:19:32 -0500

    fix basePath

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
-const repoName = '/microwave/';
+const repoName = 'microwave';
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "export",
-  assetPrefix: isProd ? repoName : '',
-  basePath: isProd ? repoName : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: isProd ? `/${repoName}` : '',
   trailingSlash: true,
   images: {
     unoptimized: true,
