70ca785f423521474b40ff085a6d2b45b9886258
bd2720 <brendandeneen9@gmail.com>
2025-03-13 16:56:18 -0400

    expand metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,36 @@ const oxygenMono = Oxygen_Mono({
   weight: "400"
 });
 
+const siteName = "Microwave Timer";
+const extendedSiteName = `${siteName} | bd2720`;
+const description = "A sleek, interactive, microwave-themed timer app created with Next.js, React, Tailwind, TypeScript & Tone.js. Setting timers on tasks can significantly increase your efficiency. Use Microwave Timer to set time limits on your daily tasks and get more done. Be sure to visit the Settings menu to customize your theme and enable sound effects!";
+const url = "https://bd2720.github.io/microwave/";
+const thumbnailUrl = `${url}microwave_thumbnail.png`;
+
 export const metadata: Metadata = {
-  title: "Microwave Timer",
-  description: "Microwave Timer application built with React and Next.js",
+  title: extendedSiteName,
+  description: description,
+  openGraph: {
+    title: extendedSiteName,
+    description: description,
+    url: url,
+    siteName: siteName,
+    images: [
+      {
+        url: thumbnailUrl,
+        width: 1200,
+        height: 630,
+        alt: "Microwave"
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: extendedSiteName,
+    description: description,
+    images: [thumbnailUrl],
+  },
 };
 
 export default function RootLayout({
